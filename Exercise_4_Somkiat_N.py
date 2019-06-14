ed78e4d4965da61bbeef1cc2932744698e5b50be
knutz32 <51819435+knutz32@users.noreply.github.com>
2019-06-14 16:36:17 +0700

    Create Exercise_4_Somkiat_N.py

---
+++ Exercise_4_Somkiat_N.py
@@ -0,0 +1,10 @@
+found_eng = 85
+general_bus = 90
+intro_com = 92
+computer_pro = 88
+
+print("--- Your Score ---")
+print("Foundation English\t\t\t\t\t :",found_eng)
+print("General Business\t\t\t\t\t :",general_bus)
+print("Introduction to Computer Systems\t :",intro_com)
+print("Computer Programming\t\t\t\t :",computer_pro)
