6f4c21708778c385bbb738f58f5a23898bcdd719
knutz32 <51819435+knutz32@users.noreply.github.com>
2019-06-19 21:25:09 +0700

    Update Exercise11_Somkiat_N.py

--- Exercise11_Somkiat_N.py
+++ Exercise11_Somkiat_N.py
@@ -1,3 +1,3 @@
 userInput = int(input("Enter number : "))
 for x in range(userInput):
-    print(" "*(userInput-(x+1))+("*"*(2*x+1))+" "*(userInput-(x+1)))
+    print(" "*(userInput-(x+1))+("*"*(2*x+1)))
