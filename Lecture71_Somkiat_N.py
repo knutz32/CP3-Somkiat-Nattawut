86281008470c30548ad1c4b2f29aabfe5405b174
knutz32 <51819435+knutz32@users.noreply.github.com>
2019-06-29 20:07:02 +0700

    Update Lecture71_Somkiat_N.py

--- Lecture71_Somkiat_N.py
+++ Lecture71_Somkiat_N.py
@@ -6,17 +6,15 @@ while True:
     if menuName.lower() == "exit":
         break
     else:
-        menuPrice = input("Enter Price : ")
+        menuPrice = int(input("Enter Price : "))
         menuList.append(menuName)
         priceList.append(menuPrice)
 
 def showBill():
-    totalPrice = 0
     print((" Food Shop ").center(30,"-"))
     for i in range(len(menuList)):
         print("%s \t\t\t %s THB" % (menuList[i],priceList[i]))
-        totalPrice += int(priceList[i])
     print("-"*30)
-    print("Total Price \t %s THB" % (totalPrice))
+    print("Total Price \t %s THB" % (sum(priceList)))
 
 showBill()
